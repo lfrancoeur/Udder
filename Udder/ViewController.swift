d227951c5d6c180a714266471244a568a838a32a
Leo Francoeur <lfrancoeur@gmail.com>
2018-10-30 10:33:26 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Leo Francoeur on 10/30/18.
+//  Copyright © 2018 Leo Francoeur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
